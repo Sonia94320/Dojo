49f2eb4c64edfd97666c3911af0a2bf5720626ac
sonia <sonia@LAPTOP-CMSJB9FI>
2021-11-30 16:09:14 +0100

    Ajoutez des fichiers projet.

---
+++ DojoService/Dojo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DojoService
+{
+    public class Dojo : IDojo
+    {
+        public List<Arme> ListerArmes()
+        {
+            return null; // new List<Arme>(); 
+        }
+        
+        public void AjouterSamourai()
+        {
+        }
+    }
+}
---
+++ DojoService/IDojo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DojoService
+{
+    [ServiceContract]
+    public interface IDojo
+    {
+        [OperationContract]
+        List<Arme> ListerArmes();
+
+        [OperationContract]
+        void AjouterSamourai();
+    }
+
+    [DataContract]
+    public class Samourai
+    {
+        private string nom;
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        [DataMember]
+        public int Force { get; set; }
+
+
+        [DataMember]
+        public Arme Arme { get; set; }
+    }
+
+    [DataContract]
+    public class Arme
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Nom { get; set; }
+        
+        [DataMember]
+        public int Degats { get; set; }
+    }
+}
